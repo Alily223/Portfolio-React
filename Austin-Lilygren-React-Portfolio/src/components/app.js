5207c1144a4ec8189f601df850e1a36b3440b572
Austin <112780937+Alily223@users.noreply.github.com>
2022-12-26 23:09:52 -0500

    infinite scroll feature v01

--- Austin-Lilygren-React-Portfolio/src/components/app.js
+++ Austin-Lilygren-React-Portfolio/src/components/app.js
@@ -13,9 +13,9 @@ import PortfolioManager from './pages/portfolio-manager';
 import PortfolioDetail from './portfolio/Portfolio-detail';
 import Auth from './pages/auth';
 import NoMatch from './pages/no-match';
-import {faTrash, faSignOutAlt, faEdit} from '@fortawesome/free-solid-svg-icons';
+import {faTrash, faSignOutAlt, faEdit , faSpinner} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faTrash, faSignOutAlt, faEdit);
+library.add(faTrash, faSignOutAlt, faEdit, faSpinner);
 
 export default class App extends Component {
   constructor(props) {
